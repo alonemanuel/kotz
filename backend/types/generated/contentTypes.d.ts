3ca7685692c71ff3be44ae3a564465ffe3544101
alonemanuel <alon@alonemanuel.com>
2024-02-05 17:27:35 +0200

    articles showing with image and content, fetched from strapi

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -362,20 +362,52 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiAboutUsAboutUs extends Schema.SingleType {
+  collectionName: 'about_uses';
+  info: {
+    singularName: 'about-us';
+    pluralName: 'about-uses';
+    displayName: 'About Us';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    body: Attribute.Text;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::about-us.about-us',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::about-us.about-us',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface ApiArticleArticle extends Schema.CollectionType {
   collectionName: 'articles';
   info: {
     singularName: 'article';
     pluralName: 'articles';
     displayName: 'Articles';
+    description: '';
   };
   options: {
     draftAndPublish: true;
   };
   attributes: {
-    article_body: Attribute.Blocks;
+    body: Attribute.Blocks;
     title: Attribute.String;
-    article_image: Attribute.Media;
+    cover: Attribute.Media;
+    author: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -615,6 +647,53 @@ export interface PluginContentReleasesReleaseAction
   };
 }
 
+export interface PluginI18NLocale extends Schema.CollectionType {
+  collectionName: 'i18n_locale';
+  info: {
+    singularName: 'locale';
+    pluralName: 'locales';
+    collectionName: 'locales';
+    displayName: 'Locale';
+    description: '';
+  };
+  options: {
+    draftAndPublish: false;
+  };
+  pluginOptions: {
+    'content-manager': {
+      visible: false;
+    };
+    'content-type-builder': {
+      visible: false;
+    };
+  };
+  attributes: {
+    name: Attribute.String &
+      Attribute.SetMinMax<
+        {
+          min: 1;
+          max: 50;
+        },
+        number
+      >;
+    code: Attribute.String & Attribute.Unique;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUsersPermissionsPermission
   extends Schema.CollectionType {
   collectionName: 'up_permissions';
@@ -766,53 +845,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-export interface PluginI18NLocale extends Schema.CollectionType {
-  collectionName: 'i18n_locale';
-  info: {
-    singularName: 'locale';
-    pluralName: 'locales';
-    collectionName: 'locales';
-    displayName: 'Locale';
-    description: '';
-  };
-  options: {
-    draftAndPublish: false;
-  };
-  pluginOptions: {
-    'content-manager': {
-      visible: false;
-    };
-    'content-type-builder': {
-      visible: false;
-    };
-  };
-  attributes: {
-    name: Attribute.String &
-      Attribute.SetMinMax<
-        {
-          min: 1;
-          max: 50;
-        },
-        number
-      >;
-    code: Attribute.String & Attribute.Unique;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -823,15 +855,16 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::about-us.about-us': ApiAboutUsAboutUs;
       'api::article.article': ApiArticleArticle;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
+      'plugin::i18n.locale': PluginI18NLocale;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
-      'plugin::i18n.locale': PluginI18NLocale;
     }
   }
 }
--- frontend/src/CensorPage.js
+++ frontend/src/CensorPage.js
@@ -1,36 +1,96 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import myImage2 from './images/kabar.jpg';
 
 
 function CensorPage() {
 
+    const [articles, setArticles] = useState([]);
+
+    // Function to render rich text body
+    const renderRichText = (body) => {
+        return body.map((paragraph, index) => (
+            <p key={index}>
+                {paragraph.children.map((child, childIndex) => (
+                    <span key={childIndex}>{child.text}</span>
+                ))}
+            </p>
+        ));
+    };
+
+    console.log(`articles:`);
+    console.log(articles);
+
     // Create an array of refs
-    const itemRefs = useRef([]);
-    itemRefs.current = Array(12).fill(null).map((_, i) => itemRefs.current[i] ?? React.createRef());
+    // const itemRefs = useRef([]);
+    // itemRefs.current = Array(12).fill(null).map((_, i) => itemRefs.current[i]
+    // ?? React.createRef());
 
     // Scroll to item function
-    const scrollToItem = (index) => {
-        itemRefs.current[index].current.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        })
-    }
+    // const scrollToItem = (index) => {
+    //     itemRefs.current[index].current.scrollIntoView({
+    //         behavior: 'smooth',
+    //         block: 'start',
+    //     })
+
+    // }
+
+    useEffect(() => {
+        // Replace with real url
+        fetch('http://localhost:1337/api/articles?populate=cover')
+            .then((Response) => Response.json())
+            .then((data) => {
+                setArticles(data.data.map(article => article.attributes));
+            })
+            .catch((error) => console.log(`Error: ${error}`));
+    }, []);
+
 
     return (
         <div className="censorPage">
             <h2>צנזורה</h2>
             <aside className="sidebar">
+                {/* Navigation - Ideally, this becomes dynamic based on fetched articles */}
+
                 <nav>
                     <ul>
-                        {Array.from({ length: 12 }, (_, i) => (
+                        {
+                            console.log('yo')}
+                        {
+                            articles.map((article, index) => (
+                                <li key={index}>
+                                    כתבה {index}
+                                </li>
+                            ))
+                        }
+                        {/* {Array.from({ length: 12 }, (_, i) => (
                             <li key={i} onClick={() => scrollToItem(i)}>
                                 אייטם {i}
                             </li>
-                        ))}
+                        ))} */}
                     </ul>
                 </nav>
             </aside>
             <main>
+                {articles.map((article, index) =>
+                (
+
+                    <article key={article.id}>
+                        <h2>
+                            {article.title}
+                        </h2>
+                        <h3>
+                            {article.author}
+                        </h3>
+                        <p>
+                            {renderRichText(article.body)}
+                        </p>
+                        <img src={`http://localhost:1337${article.cover?.data?.attributes.url}`} alt="" />
+                        <hr />
+                    </article>
+                )
+                )}
+            </main>
+            {/* <main>
                 {Array.from({ length: 12 }, (_, i) => (
                     <div ref={itemRefs.current[i]} key={i} className="contentItem">
 
@@ -43,7 +103,7 @@ function CensorPage() {
                     </div>
                 ))}
                 <img src={myImage2} alt="הקבר המזמר" className="magazine-image" />
-            </main>
+            </main> */}
         </div>
     );
 }
