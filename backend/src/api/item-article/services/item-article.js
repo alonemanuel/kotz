6926981d794a3ac7fad193b5d0cbe771b0c46d17
alonemanuel <alon@alonemanuel.com>
2024-03-05 07:08:56 +0200

    Position of header is now sticky and scrolls

---
+++ backend/src/api/item-article/services/item-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * item-article service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::item-article.item-article');
--- frontend/src/components/Accordion.tsx
+++ frontend/src/components/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "../styles/Accordion.module.css";
 import { Item } from "./ItemInterface";
 
@@ -8,9 +8,21 @@ interface AccordionProps {
 
 const Accordion: React.FC<AccordionProps> = ({ articles }) => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  // Create a ref array for each accordion item
+  const accordionRefs = useRef<(HTMLButtonElement | null)[]>([]);
 
   const toggleAccordion = (index: number) => {
     setActiveIndex(activeIndex === index ? null : index);
+
+    // If the accordion is being opened, scroll it into view
+    if (activeIndex !== index) {
+      setTimeout(() => {
+        accordionRefs.current[index]?.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }, 300);
+    }
   };
 
   return (
@@ -18,6 +30,7 @@ const Accordion: React.FC<AccordionProps> = ({ articles }) => {
       {articles.map((article, index) => (
         <React.Fragment key={article.id}>
           <button
+            ref={(el) => (accordionRefs.current[index] = el)} // Assign ref to button
             className={`${styles.accordion}  ${
               activeIndex === index ? styles.active : ""
             }`}
