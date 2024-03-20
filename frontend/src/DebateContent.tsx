ae5cf082092c37096b2b78f7b6157f37402f7a93
alonemanuel <alon@alonemanuel.com>
2024-03-20 21:48:21 +0200

    Toggle debates working

--- frontend/src/DebateContent.tsx
+++ frontend/src/DebateContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Paragraph from "./Paragraph";
 import BoldText from "./BoldText";
 import ItalicText from "./ItalicText";
@@ -8,10 +8,23 @@ import styles from "./styles/CensorshipPage.module.css";
 import ArticleContent from "./ArticleContent";
 
 const DebateContent: React.FC<{ content?: any[] }> = ({ content }) => {
+  const [activeDebate, setActiveDebate] = useState<number>(0);
+  const debateRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const toggleDebate = (index: number) => {
+    setActiveDebate(index);
+  };
+
   return (
     <div className={styles.debates}>
-      {content?.map((content) => (
-        <div className={styles.debate}>
+      {content?.map((content, index) => (
+        <div
+          ref={(el) => (debateRefs.current[index] = el)}
+          onClick={() => toggleDebate(index)}
+          className={`${styles.debate} ${
+            activeDebate === index ? styles.active : ""
+          }`}
+        >
           <img src={content?.author_img?.data?.attributes?.url} alt="bla bla" />
           <h1>{content?.author}</h1>
           <h2>{content?.lead}</h2>
