a13f750a13e3cf2808d3781d5f8ed9dec8495b3d
alonemanuel <alon@alonemanuel.com>
2024-03-21 04:38:31 +0200

    Testing touch

--- frontend/src/PollContent.tsx
+++ frontend/src/PollContent.tsx
@@ -20,47 +20,61 @@ const PollContent: React.FC<{ content?: any; cover?: any }> = ({
 // Make the DIV element draggable:
 // dragElement(document.getElementById("mydiv"));
 
-function dragElement(elmnt:HTMLElement) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-//   if (document.getElementById(elmnt.id + "header")) {
-//     // if present, the header is where you move the DIV from:
-//     elmnt.onmousedown = dragMouseDown;
-//   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-//   }
-
-  function dragMouseDown(e:MouseEvent) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    elmnt.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    elmnt.onmousemove = (e) => elementDrag(elmnt, e);
+function dragElement(elmnt: HTMLElement) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  
+    const dragMouseDown = (e: MouseEvent | TouchEvent) => {
+      e.preventDefault();
+      // Determine whether the event is a touch event
+      if (e.type === 'touchstart' && e instanceof TouchEvent) {
+        // Use the first touch point
+        pos3 = e.touches[0].clientX;
+        pos4 = e.touches[0].clientY;
+      } else if (e instanceof MouseEvent) {
+        // Use the mouse event for non-touch devices
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+      }
+      document.onmouseup = closeDragElement;
+      document.ontouchend = closeDragElement;
+      document.onmousemove = (e) => elementDrag(e);
+      document.ontouchmove = (e) => elementDrag(e);
+    };
+  
+    const elementDrag = (e: MouseEvent | TouchEvent) => {
+      e.preventDefault();
+      // Determine whether the event is a touch event
+      if (e.type === 'touchmove' && e instanceof TouchEvent) {
+        // Use the first touch point for touch devices
+        pos1 = pos3 - e.touches[0].clientX;
+        pos2 = pos4 - e.touches[0].clientY;
+        pos3 = e.touches[0].clientX;
+        pos4 = e.touches[0].clientY;
+      } else if (e instanceof MouseEvent) {
+        // Use the mouse event for non-touch devices
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+      }
+      // Set the element's new position
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    };
+  
+    const closeDragElement = () => {
+      // Stop moving when mouse button is released or touch ends
+      document.onmouseup = null;
+      document.ontouchend = null;
+      document.onmousemove = null;
+      document.ontouchmove = null;
+    };
+  
+    // Add event listeners for both mouse and touch events
+    elmnt.addEventListener('mousedown', dragMouseDown);
+    elmnt.addEventListener('touchstart', dragMouseDown);
   }
-
-  function elementDrag(elmnt:HTMLElement, e:MouseEvent) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    elmnt.onmouseup = null;
-    elmnt.onmousemove = null;
-  }
-}
-
+  
 
 
 
