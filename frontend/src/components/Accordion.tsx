b3d6a315b72ac6a4c7cd7f481ae1dae3a0e1d61f
alonemanuel <alon@alonemanuel.com>
2024-03-22 14:10:35 +0200

    Fix resizing

--- frontend/src/components/Accordion.tsx
+++ frontend/src/components/Accordion.tsx
@@ -9,39 +9,70 @@ import ArticleContent from "../ArticleContent";
 import ArticleComponent from "../ArticleComponent";
 import { useOpenArticle } from "../OpenArticleContext";
 import userEvent from "@testing-library/user-event";
-
 interface AccordionProps {
   // articles: Item[];
   articles: Article[];
   terms: Term[];
 }
-function useResizeObservers(refs: any) {
+
+function debounce(func: any, wait: number) {
+  let timeout: any;
+
+  return function exectuedFunction(...args: any) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
+function useResizeObservers(refs: any, dependency: number | null) {
   const [dimensions, setDimensions] = useState([]);
 
   useEffect(() => {
+    const handleResize = debounce(() => {
+      console.log(`Handling resize`);
+      setDimensions(
+        refs.current.map((ref: any) => {
+          return ref
+            ? {
+                width: ref.clientWidth,
+                height: ref.clientHeight,
+              }
+            : { width: 0, height: 0 };
+        })
+      );
+    }, 1000);
+
     const resizeObservers = refs.current.map((ref: any, index: number) => {
-      const observer = new ResizeObserver((entries) => {
-        entries.forEach((entry) => {
-          setDimensions((prevDimensions) => {
-            const newDimensions: any = [...prevDimensions];
-            newDimensions[index] = {
-              width: entry.contentRect.width,
-              height: entry.contentRect.height,
-            };
-            return newDimensions;
-          });
-        });
+      const observer = new ResizeObserver(() => {
+        handleResize();
+        setTimeout(() => {
+          handleResize();
+        }, 2000);
       });
       if (ref) observer.observe(ref);
       return observer;
     });
 
+    window.addEventListener("resize", handleResize);
+
+    // Initial resize handling
+    handleResize();
+    setTimeout(() => {
+      handleResize();
+    }, 2000);
+
+    // Cleanup
     return () => {
       resizeObservers.forEach((observer: any, index: number) => {
         if (refs.current[index]) observer.unobserve(refs.current[index]);
       });
     };
-  }, [refs]);
+  }, [refs, dependency]);
 
   return dimensions;
 }
@@ -49,7 +80,7 @@ const Accordion: React.FC<AccordionProps> = ({ articles, terms }) => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
   // Create a ref array for each accordion item
   const accordionRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const dimensions = useResizeObservers(accordionRefs);
+  const dimensions = useResizeObservers(accordionRefs, activeIndex);
 
   const panelRefs = useRef<(HTMLDivElement | null)[]>([]);
   const textContentHeights = useRef<(number | null)[]>([]);
