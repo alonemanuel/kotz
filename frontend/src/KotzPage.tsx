1e79de9dfea5da130f2251f4e4cac1b81923062e
Asaf Arad <65238226+asafarad1@users.noreply.github.com>
2024-03-29 13:07:42 +0300

    asaf_01

--- frontend/src/KotzPage.tsx
+++ frontend/src/KotzPage.tsx
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../src/styles/KotzPage.module.css";
 import kabarImg from "./images/kabar.jpg";
-import kotzImg from "./images/kotz.svg";
+import kotzImg1 from "./images/kotz.svg";
+import kotzImg2 from "./images/kotz2.svg";
+import kotzImg3 from "./images/kotz3.svg";
+import kotzImg4 from "./images/kotz4.svg";
+import kotzImg5 from "./images/kotz5.svg";
+import kotzImg6 from "./images/kotz6.svg";
 import kotzImgWhite from "./images/kotz-white.svg";
 
 import KotzFab from "./KotzFab";
@@ -21,12 +26,12 @@ const KotzPage: React.FC = () => {
   const navigate = useNavigate();
 
   const boxes: Box[] = [
-    { id: 0, title: "צנזורה", image: kotzImg, path: "/censorship" },
-    { id: 1, image: kotzImg },
-    { id: 2, image: kotzImg },
-    { id: 3, image: kotzImg },
-    { id: 4, image: kotzImg },
-    { id: 5, image: kotzImg },
+    { id: 0, title: "צנזורה", image: kotzImg1, path: "/censorship" },
+    { id: 1, image: kotzImg2 },
+    { id: 2, image: kotzImg3 },
+    { id: 3, image: kotzImg4 },
+    { id: 4, image: kotzImg5 },
+    { id: 5, image: kotzImg6 },
   ];
 
   const handleBoxClick = (path?: string) => {
