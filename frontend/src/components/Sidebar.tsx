39815365355db2a27bf65e62a211c83c5b1873e7
alonemanuel <alon@alonemanuel.com>
2024-06-15 18:33:04 +0300

    Remove get rand color

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -16,15 +16,6 @@ interface SidebarProps {
   terms: Term[];
 }
 
-const getRandomColor = () => {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
 function debounce(func: any, wait: number) {
   let timeout: any;
 
