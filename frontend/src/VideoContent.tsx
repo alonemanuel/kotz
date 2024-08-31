d750642562ad04001b4bc0e7877638379c924ac8
alonemanuel <alon@alonemanuel.com>
2024-08-31 16:54:21 +0300

    Video appearing nicely

--- frontend/src/VideoContent.tsx
+++ frontend/src/VideoContent.tsx
@@ -1,13 +1,13 @@
 import React, { useRef, useState } from "react";
 import JsonBlocksContent from "./JsonBlocksContent";
-import styles from "./styles/CensorshipPage.module.css";
+import styles from "./styles/FakePage.module.css";
 
 const VideoContent: React.FC<{ content?: any }> = ({ content }) => {
   return (
     <main className={styles.video}>
       <div className={styles.videoContainer}>
         <iframe
-          width="100%"
+          width="560"
           height="315"
           src={content} // Assumes video URL is provided in attr.videoUrl
           title="YouTube video player"
@@ -16,6 +16,7 @@ const VideoContent: React.FC<{ content?: any }> = ({ content }) => {
           allowFullScreen
         ></iframe>
       </div>
+      <div className={styles.videoFooter}></div>
     </main>
   );
 };
